053e4dd10e68881eb72cc901b4918aec7bfed70c
root <root@Home1.localdomain>
2019-12-03 21:31:33 +0100

    spike nats

---
+++ app-notifications-with-nats/src/main/java/net/siudek/spike/NativeListener.java
@@ -0,0 +1,8 @@
+package net.siudek.spike;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class NativeListener {
+
+}
---
+++ app-notifications-with-nats/src/test/java/net/siudek/spike/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.siudek.spike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

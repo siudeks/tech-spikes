212d5404b04b4a37c631aa14c9c801c096453017
Ubuntu <siudeks@siudeksdevmachine.43t202pckkdedgyhbz44bqyqsg.ax.internal.cloudapp.net>
2020-02-16 09:29:51 +0000

    WIP

--- azure-protected-api-spring-boot/src/main/java/com/example/demo/HttpSecurityConfigurer.java
+++ azure-protected-api-spring-boot/src/main/java/com/example/demo/HttpSecurityConfigurer.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 // @EnableWebSecurity

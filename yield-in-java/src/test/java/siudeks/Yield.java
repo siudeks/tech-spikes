1388cae60bc2a90f71ea0c9027b4d5b3f981f89c
Slawomir Siudek <siudeks@gmail.com>
2020-06-06 10:26:34 +0200

    WIP

--- yield-in-java/src/test/java/siudeks/Yield.java
+++ yield-in-java/src/test/java/siudeks/Yield.java
@@ -2,4 +2,22 @@ package siudeks;
 
 public class Yield {
     
+    public static Iterable<T> of(final Body body) {
+
+    }
+
+    public abstract class Body<T> {
+        protected T onNext() {
+
+        }
+
+        protected T onComplete() {
+
+        }
+
+        void break() {
+            
+        }
+    }
+
 }

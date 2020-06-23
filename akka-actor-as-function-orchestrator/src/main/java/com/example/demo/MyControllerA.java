22600bbb4ba374fa8ac21473c83026dcbe052399
Slawomir Siudek <siudeks@gmail.com>
2020-06-23 19:05:40 +0200

    WIP

---
+++ akka-actor-as-function-orchestrator/src/main/java/com/example/demo/MyControllerA.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import akka.actor.typed.scaladsl.Behaviors;
+
+@RestController
+public class MyControllerA {
+
+    @Autowired
+    private AsyncMediatorRunner runner;
+
+    @GetMapping(path = "/a")
+    public CompletionStage<String> hello() {
+
+        var result = new CompletableFuture<String>();
+        runner.spawn(Behaviors.empty(), () -> {
+            result.complete("empty!");
+        }, Duration.ofSeconds(5));
+
+        return result
+            .thenApply(it -> it.toString());
+    }
+}

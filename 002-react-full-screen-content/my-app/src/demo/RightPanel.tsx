948d82386bb5af34f30694e74cbf574fd9f58385
Slawomir Siudek <slawomir@siudek.net>
2021-11-22 20:50:21 +0100

    Formatting

--- 002-react-full-screen-content/my-app/src/demo/RightPanel.tsx
+++ 002-react-full-screen-content/my-app/src/demo/RightPanel.tsx
@@ -8,12 +8,8 @@ export const RightPanel: React.FC<{}> = props => {
     return (
         < Stack
             styles={{
-                root: {
-                    width: "100%",
-                    height: "100%",
-                },
-            }}
-        >
+                root: { width: "100%", height: "100%" }
+            }}>
             <Stack.Item verticalFill>
                 <Stack
                     styles={{

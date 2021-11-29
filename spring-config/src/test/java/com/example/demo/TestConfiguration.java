695384144c5de8d00e1df7b2b4917d8d229f27ae
Slawomir Siudek <slawomir@siudek.net>
2021-11-29 11:16:20 +0100

    Minimized tests configuration

--- spring-config/src/test/java/com/example/demo/TestConfiguration.java
+++ spring-config/src/test/java/com/example/demo/TestConfiguration.java
@@ -6,15 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @TestPropertySource(locations = "classpath:application-ci.properties")
 @EnableConfigurationProperties(PersonConfig.class)
-@ContextConfiguration(
-    classes = { PersonConfig.class})
 public class TestConfiguration {
 
     @Autowired

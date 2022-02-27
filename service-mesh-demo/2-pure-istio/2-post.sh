9893d7a80b35f752a83ff2afc7b4d67480061632
Slawomir Siudek <slawomir@siudek.net>
2022-02-27 21:54:38 +0100

    WIP

--- service-mesh-demo/2-pure-istio/2-post.sh
+++ service-mesh-demo/2-pure-istio/2-post.sh
@@ -1,1 +1,9 @@
 kubectl delete -f deployment.yaml
+
+export NAMESPACE=demo0
+export ISTIO_DIR=istio-1.12.2
+export PATH=$PWD/$ISTIO_DIR/bin:$PATH
+
+kubectl delete -f $ISTIO_DIR/samples/addons
+
+./istio-1.12.2/bin/istioctl x uninstall --purge -y

96d24c241c12df6305c5f9455daf4838f0fc97ea
Slawomir Siudek <slawomir@siudek.net>
2022-03-09 21:32:50 +0100

    Example of multimodule proto

--- bugrepro_grpc_mutiny/grpc-client/src/main/java/MyClass.java
+++
@@ -1,6 +0,0 @@
-import io.quarkus.example.HelloRequest;
-
-// The class should be compilable as referenced proto class is generated
-public class MyClass {
-    public HelloRequest someProtoMessage = HelloRequest.newBuilder().build();
-}

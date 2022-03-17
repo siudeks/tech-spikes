3499a7463361b39fd0daa154092bb850faa88248
Slawomir Siudek <slawomir@siudek.net>
2022-03-17 11:37:54 +0100

    Working example with fallback bean

--- 005-spring-alternative-beans-2/src/main/java/com/example/demo/MyBeanConfigurerFallback.java
+++ 005-spring-alternative-beans-2/src/main/java/com/example/demo/MyBeanConfigurerFallback.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 public class MyBeanConfigurerFallback {
     
     @Bean
+    @ConditionalOnMissingBean
     MyBean myFallbackBean() {
         MyBean.testCounter++;
         return new MyBean("my fallback bean");

0fd4a4da28f5c607dadf862cdc38617fb7e52ed2
Slawomir Siudek <slawomir@siudek.net>
2023-04-15 13:47:52 +0200

    Remove unused project

--- 002-react-full-screen-content/my-app/src/App.tsx
+++
@@ -1,6 +0,0 @@
-import './App.css';
-import { Demo } from './demo/Demo';
-
-export const App: React.FC<{}> = props => {
-  return (<Demo />);
-}
--- 002-react-full-screen-content/my-app/src/demo/RightBottomPanel.tsx
+++
@@ -1,7 +0,0 @@
-import { Label } from "@fluentui/react";
-
-export const RightBottomPanel: React.FC<{ }> = props => {
-    return (
-        <Label>Right Bottom Panel</Label>
-    );
-}

04f6d798bc829a7b9da427b9f4e40d1a37fed38a
siudeks <slawomir@siudek.net>
2024-11-04 20:15:55 +0100

    WIP

--- java-long/src/test/java/com/example/demo/LongTest.java
+++ java-long/src/test/java/com/example/demo/LongTest.java
@@ -2,11 +2,16 @@ package com.example.demo;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.LongAdder;
 import java.util.stream.IntStream;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 
 import lombok.SneakyThrows;
 
@@ -14,7 +19,10 @@ public class LongTest {
 
     @Test
     @SneakyThrows
-    void aaa() throws InterruptedException {
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @BenchmarkMode(Mode.AverageTime)
+    public void aaa() throws InterruptedException {
         var counter = new LongAdder();
         var executorService = Executors.newFixedThreadPool(8);
 
@@ -26,12 +34,13 @@ public class LongTest {
                 .forEach(i -> counter.increment());
 
         var tasks = IntStream
-            .range(0, numberOfThreads).mapToObj(index -> incrementAction)
-            .map(Executors::callable)
-            .toList();
+                .range(0, numberOfThreads).mapToObj(index -> incrementAction)
+                .map(Executors::callable)
+                .toList();
 
         var waiter = executorService.invokeAll(tasks);
-        for (var task: waiter) task.get();
+        for (var task : waiter)
+            task.get();
 
         Assertions.assertThat(counter.sum()).isEqualTo(numberOfIncrements * numberOfThreads);
     }
